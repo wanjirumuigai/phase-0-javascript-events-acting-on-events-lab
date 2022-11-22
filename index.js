e65f6994c48beae8d8a6779c2a3e5f24d3fad6d8
Pauline Muigai <wanjirumuigai@gmail.com>
2022-11-22 12:45:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+ function moveDodgerRight() {
+    const rightNumber = dodger.style.left.replace("px","");
+    const right = parseInt(rightNumber,10);
+
+   if (right > 0 && right < 360) {
+        dodger.style.left = `${right + 1}px`;
+     }
+    
+ }  
+ document.addEventListener("keydown",function (e) {
+    if (e.key==="ArrowRight") {
+        moveDodgerRight();
+    }
+ })
