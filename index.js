4302ad11b8406f50b09522eb81a56eff97456fc3
Pauline Muigai <wanjirumuigai@gmail.com>
2022-12-14 08:59:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Your code here
-function moveDodgerLeft() {
+
+const dodger = document.getElementById("dodger");
+
+
+  function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
@@ -7,25 +11,23 @@ function moveDodgerLeft() {
       dodger.style.left = `${left - 1}px`;
     }
   }
-  
-  
-  
+
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
   });
- function moveDodgerRight() {
-    const rightNumber = dodger.style.left.replace("px","");
-    const right = parseInt(rightNumber,10);
+  
+  function moveDodgerRight() {
+    const rightNumbers =dodger.style.left.replace("px","");
+    const right = parseInt(rightNumbers,10);
 
-   if (right > 0 && right < 360) {
-        dodger.style.left = `${right + 1}px`;
-     }
-    
- }  
- document.addEventListener("keydown",function (e) {
-    if (e.key==="ArrowRight") {
-        moveDodgerRight();
+    if (right>=0 && right<360) {
+      dodger.style.left = `${right + 1}px`;
     }
- })
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
